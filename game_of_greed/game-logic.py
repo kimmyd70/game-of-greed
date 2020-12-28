926530530ae1a2713aed8b949af90c805a0d4cbf
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-12-28 13:08:39 -0800

    create game_logic.py

---
+++ game_of_greed/game-logic.py
@@ -0,0 +1,24 @@
+class GameLogic:
+    
+    @static
+    def calculate_score(roll:tuple):
+        # calculate the roll's score using rules of the game
+    pass
+        return score_from_roll
+    
+    @static
+    def roll_dice(die_num:int):
+        # create tuple with random int 1-6
+        # The length of tuple must match the argument given 
+        # to roll_dice method???
+
+class Banker:
+    def shelf(add_to_shelf:int):
+        #temp store unbanked points
+        
+    def bank():
+        #add points on shelf to total
+        #reset shelf to 0
+        
+    def clear_shelf():
+        #remove all unbanked points
