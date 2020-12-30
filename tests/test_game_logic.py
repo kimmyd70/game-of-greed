aad4d20a2e83cd6ffc6dd07ca08b2ddf0c6f1713
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-12-29 18:26:37 -0800

    test roll_dice for results between 1-6

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -21,15 +21,27 @@ def test_roll_dice_length():
     assert actual == expected
     
 # 2) Each item in sequence is an integer with value between 1 and 6
-# passes: but this is a crap test
+# passes: broken into separate verifications
 def test_roll_dice_int():
     our_tuple = roll_dice(4)
     result = 'types'
-    for item in range (0,3):
+    for item in range (0,4):
         result = f'{result} : {type(our_tuple[item])}'
     actual = result
-    expected = "types : <class 'int'> : <class 'int'> : <class 'int'>" 
+    expected = "types : <class 'int'> : <class 'int'> : <class 'int'> : <class 'int'>" 
     assert actual == expected
+    
+def test_roll_dice_range():
+    our_second_tuple = roll_dice(4)
+    result = 'result:'
+    for item in range (0,4):
+        if 1 <= our_second_tuple[item] <= 6:
+            result = f'{result} Ok'
+            
+    actual = result
+    expected = "result: Ok Ok Ok Ok" 
+    assert actual == expected
+
 
 ### Testing - Calculate Score
 
